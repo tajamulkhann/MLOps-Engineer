c7e00422fff0fc9eb52d53d63ec60cc42962fff5
Tajamul Khan <tajamulk@gmail.com>
2025-11-08 17:50:14 +0530

    add

--- Network Security Project/networksecurity/logging/logger.py
+++ Network Security Project/networksecurity/logging/logger.py
@@ -2,15 +2,19 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# Generate a unique log file name based on the current timestamp  
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+# Create a 'logs' directory inside the current working directory and store logs there
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(logs_path, exist_ok=True)  # Create the directory if it doesn't exist
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# Full path to store the log file
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
+# Configure the logging settings
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+    filename=LOG_FILE_PATH,  # File to save logs
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",  # Log format
+    level=logging.INFO,  # Minimum level of logs to be captured
 )
